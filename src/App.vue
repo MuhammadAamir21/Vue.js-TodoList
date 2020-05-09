b0f66e875d7c82a76b677bf0cff1152ec110c276
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-05-10 02:28:14 +0500

    Add and view TodoList components created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
+  <!-- We can only have one div -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <!-- Catching the emitted data from AddTodo -->
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
-
+ <!-- Register any component in the componets sections-->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/Header";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    //here we return an arrays of objects
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      //her filter loop through like an foreach loop and remove the data related to the id
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      //Adding the new todo object in the end
+      this.todos=[...this.todos,newTodo];
+
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* The is global style sheet */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn.hover{
+  background: #666;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <!-- .stop.prevent prevent it from reloading the page -->
+        <form @submit="addTodo">
+        <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+<script>
+//import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+    name:"AddTodo",
+    data() {
+        return {
+            title:''
+        }
+    },
+    methods: {
+        addTodo(e){
+            //Work same as
+            //.stop.prevent prevent it from reloading the page
+            e.preventDefault();
+            
+            const newTodo ={
+                //npm i uuid
+                //A simple unique id generator
+                id: uuidv4(),
+                title: this.title,
+                completed: false
+            }
+            //Send up to parent
+            this.$emit('add-todo',newTodo);
+            this.title='';
+        }
+    },
+}
+</script>
+<style scoped>
+form{
+    display: flex;
+}
+input[type="text"]{
+    flex: 10;
+    padding: 5px;
+}
+input[type="submit"]{
+    flex: 2;
+}
+</style>
